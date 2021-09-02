82e964df1d7d7aae10905c05e07794a6b356a438
NicolasMetivier-at-162491939266 <metivier.nicolas@engie.com>
2021-09-02 23:49:21 +0200

    Transform to multi micro service architecture

---
+++ ride4ever-ride/src/main/java/io/nirahtech/ride4ever/ride/NirahRide4EverRideApplication.java
@@ -0,0 +1,17 @@
+/******************************************************************
+ * Copyright 2021 Ride4Ever
+ * 
+ * TO BE DEFINED
+ ******************************************************************/
+package io.nirahtech.ride4ever.ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableEurekaClient
+public class NirahRide4EverRideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NirahRide4EverRideApplication.class, args);
+     }
+}
